de4a32992d381a98ddfec87e5a8b7c9218b6c49b
ViugiNick <viuginov.nickolay@gmail.com>
2014-12-12 23:19:59 +0400

    TAPPPPPS

--- keypressfilter.cpp
+++ keypressfilter.cpp
@@ -5,105 +5,105 @@ KeyPressFilter *KeyPressFilter::instance = 0;
 
 void KeyPressFilter::addPoint(float x, float y)
 {
-    minX = std::min(x, minX);
-    maxX = std::max(x, maxX);
+	minX = std::min(x, minX);
+	maxX = std::max(x, maxX);
 
-    minY = std::min(y, minY);
-    maxY = std::max(y, maxY);
+	minY = std::min(y, minY);
+	maxY = std::max(y, maxY);
 
-    allPoints.append(qMakePair(x, y));
+	allPoints.append(qMakePair(x, y));
 }
 
 void KeyPressFilter::clearMatrix()
 {
-    allPoints.clear();
-    minX = 10000, maxX = -10000, minY = 10000, maxY = -10000;
-    for(int j = 0; j < 10; j++)
-    {
-        for(int i = 0; i < 10; i++)
-        {
-            mapOfGestures[i][j] = 0;
-        }
-    }
+	allPoints.clear();
+	minX = 10000, maxX = -10000, minY = 10000, maxY = -10000;
+	for(int j = 0; j < 10; j++)
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			mapOfGestures[i][j] = 0;
+		}
+	}
 }
 
 bool KeyPressFilter::vectorEquals(const QVector < QPair<int, int> > & v1, const QVector < QPair<int, int> > & v2)
 {
-    return(min(v1.size(), v2.size()) >= 10 * levenshtein_distance(v1, v2));
+	return(min(v1.size(), v2.size()) >= 10 * levenshtein_distance(v1, v2));
 }
 
 void KeyPressFilter::findMacros()
 {
 
-    minX -= 10;
-    minY -= 10;
+	minX -= 10;
+	minY -= 10;
 
-    maxX += 10;
-    maxY += 10;
+	maxX += 10;
+	maxY += 10;
 
-    float dx = (maxX - minX)/10;
-    float dy = (maxY - minY)/10;
+	float dx = (maxX - minX)/10;
+	float dy = (maxY - minY)/10;
 
-    int indexOfBox = 0;
+	int indexOfBox = 0;
 
-    keyVector.clear();
+	keyVector.clear();
 
-    for (int j = 0; j < allPoints.size(); ++j)
-    {
-        int idX = -1;
-        int idY = -1;
-        //qDebug() << allPoints.at(j).first << " " << allPoints.at(j).second;
-        for(int i = 0; i < 10; i++)
-            if(minX + i * dx <= allPoints.at(j).first && minX + (i + 1) * dx >= allPoints.at(j).first)
-            {
-                idX = i;
-            }
-        for(int i = 0; i < 10; i++)
-            if(minY + i * dy <= allPoints.at(j).second && allPoints.at(j).second <= minY + (i + 1) * dy)
-            {
-                idY = i;
-            }
-        if(idX != -1 && idY != -1)
-        {
-            if(mapOfGestures[idX][idY] == 0)
-            {
-                indexOfBox++;
-                keyVector.append(qMakePair(idX, idY));
-                std::cerr << idX << " " << idY << std::endl;
-            }
-            mapOfGestures[idX][idY] = indexOfBox;
-        }
-        //qDebug() << idX << " " << idY;
-    }
+	for (int j = 0; j < allPoints.size(); ++j)
+	{
+		int idX = -1;
+		int idY = -1;
+		//qDebug() << allPoints.at(j).first << " " << allPoints.at(j).second;
+		for(int i = 0; i < 10; i++)
+			if(minX + i * dx <= allPoints.at(j).first && minX + (i + 1) * dx >= allPoints.at(j).first)
+			{
+				idX = i;
+			}
+		for(int i = 0; i < 10; i++)
+			if(minY + i * dy <= allPoints.at(j).second && allPoints.at(j).second <= minY + (i + 1) * dy)
+			{
+				idY = i;
+			}
+		if(idX != -1 && idY != -1)
+		{
+			if(mapOfGestures[idX][idY] == 0)
+			{
+				indexOfBox++;
+				keyVector.append(qMakePair(idX, idY));
+				std::cerr << idX << " " << idY << std::endl;
+			}
+			mapOfGestures[idX][idY] = indexOfBox;
+		}
+		//qDebug() << idX << " " << idY;
+	}
 
-    //FOREACH MACROS
-    tempVector.clear();
-    tempVector.append(qMakePair(0, 5));
-    tempVector.append(qMakePair(1, 5));
-    tempVector.append(qMakePair(2, 5));
-    tempVector.append(qMakePair(3, 5));
-    tempVector.append(qMakePair(4, 5));
-    tempVector.append(qMakePair(5, 5));
-    tempVector.append(qMakePair(6, 5));
-    tempVector.append(qMakePair(7, 5));
-    tempVector.append(qMakePair(8, 5));
-    tempVector.append(qMakePair(9, 5));
+	//FOREACH MACROS
+	tempVector.clear();
+	tempVector.append(qMakePair(0, 5));
+	tempVector.append(qMakePair(1, 5));
+	tempVector.append(qMakePair(2, 5));
+	tempVector.append(qMakePair(3, 5));
+	tempVector.append(qMakePair(4, 5));
+	tempVector.append(qMakePair(5, 5));
+	tempVector.append(qMakePair(6, 5));
+	tempVector.append(qMakePair(7, 5));
+	tempVector.append(qMakePair(8, 5));
+	tempVector.append(qMakePair(9, 5));
 
-    if(vectorEquals(tempVector, keyVector))
-    {
-        std::cerr << "Macros" << std::endl;
-    }
+	if(vectorEquals(tempVector, keyVector))
+	{
+		std::cerr << "Macros" << std::endl;
+	}
 
-    //--------------
+	//--------------
 
-    for(int j = 0; j < 10; j++)
-    {
-        for(int i = 0; i < 10; i++)
-        {
-            std::cerr << mapOfGestures[i][j] << " ";
-        }
-        qDebug();
-    }
+	for(int j = 0; j < 10; j++)
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			std::cerr << mapOfGestures[i][j] << " ";
+		}
+		qDebug();
+	}
 }
 
 LRESULT CALLBACK KeyPressFilter::MyLowLevelKeyBoardProc(int nCode, WPARAM wParam, LPARAM lParam) {
@@ -149,39 +149,39 @@ LRESULT CALLBACK KeyPressFilter::MyLowLevelKeyBoardProc(int nCode, WPARAM wParam
 
 LRESULT CALLBACK KeyPressFilter::MyLowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    if(wParam == WM_LBUTTONDOWN)
-    {
-        getInstance()->clearMatrix();
-        qDebug() << "Mouse Pressed!";
+	if(wParam == WM_LBUTTONDOWN)
+	{
+		getInstance()->clearMatrix();
+		qDebug() << "Mouse Pressed!";
 
-        getInstance()->mouseDown();
+		getInstance()->mouseDown();
 
-        tagMSLLHOOKSTRUCT cMouse = *((tagMSLLHOOKSTRUCT*)lParam);
-        qDebug() << cMouse.pt.x << " " << cMouse.pt.y;
-        getInstance()->addPoint(cMouse.pt.x, cMouse.pt.y);
-    }
+		tagMSLLHOOKSTRUCT cMouse = *((tagMSLLHOOKSTRUCT*)lParam);
+		qDebug() << cMouse.pt.x << " " << cMouse.pt.y;
+		getInstance()->addPoint(cMouse.pt.x, cMouse.pt.y);
+	}
 
-    if(wParam == WM_LBUTTONUP)
-    {
-        qDebug() << "Mouse UP!";
+	if(wParam == WM_LBUTTONUP)
+	{
+		qDebug() << "Mouse UP!";
 
-        getInstance()->mouseUp();
+		getInstance()->mouseUp();
 
-        tagMSLLHOOKSTRUCT cMouse = *((tagMSLLHOOKSTRUCT*)lParam);
-        qDebug() << cMouse.pt.x << " " << cMouse.pt.y;
+		tagMSLLHOOKSTRUCT cMouse = *((tagMSLLHOOKSTRUCT*)lParam);
+		qDebug() << cMouse.pt.x << " " << cMouse.pt.y;
 
-        getInstance()->findMacros();
-    }
+		getInstance()->findMacros();
+	}
 
-    if(wParam == WM_MOUSEMOVE && getInstance()->mousePressed)
-    {
-        qDebug() << "Mouse Moved!";
+	if(wParam == WM_MOUSEMOVE && getInstance()->mousePressed)
+	{
+		qDebug() << "Mouse Moved!";
 
-        tagMSLLHOOKSTRUCT cMouse = *((tagMSLLHOOKSTRUCT*)lParam);
-        qDebug() << cMouse.pt.x << " " << cMouse.pt.y;
+		tagMSLLHOOKSTRUCT cMouse = *((tagMSLLHOOKSTRUCT*)lParam);
+		qDebug() << cMouse.pt.x << " " << cMouse.pt.y;
 
-        getInstance()->addPoint(cMouse.pt.x, cMouse.pt.y);
-    }
+		getInstance()->addPoint(cMouse.pt.x, cMouse.pt.y);
+	}
 
-    return CallNextHookEx(getInstance()->hHookMouse, nCode, wParam, lParam);
+	return CallNextHookEx(getInstance()->hHookMouse, nCode, wParam, lParam);
 }
